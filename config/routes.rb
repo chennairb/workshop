76040d733992f8ddd25852bd71d78147f0f8c9e2
abhishek <bigbeliever@gmail.com>
2014-07-20 16:32:51 +0530

    Added bootswatch superhero theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  resources :coaches
 
+
+  resources :coaches
   resources :participants
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
