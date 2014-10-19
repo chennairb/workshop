759de3ee4c06a876006ddb5c881e66ab015e4251
abhishek <bigbeliever@gmail.com>
2014-10-19 13:17:59 +0530

    new UI

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,16 @@
 class WelcomeController < ApplicationController
 
-	http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASS'], except: :index
+	http_basic_authenticate_with name: ENV['ADMIN_USER'], 
+	  password: ENV['ADMIN_PASS'], 
+	  except: [:index, :home]
 
 	def index
 		@participant = Participant.new
 		@coach = Coach.new
+	end
+
+	def home
+		@participant = Participant.new
 	end
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   resources :coaches
   resources :participants
-  root 'welcome#index'
+  #root 'welcome#index'
+  root 'welcome#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
