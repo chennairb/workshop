e064d2c65de44915744168d07fe7e2e9aab0db70
Vijay Raghavan Aravamudhan <avijayr - at - gmail - dot - com>
2015-01-15 12:55:26 +0530

    Run the annotate gem on all models.

--- test/models/coach_test.rb
+++ test/models/coach_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: coaches
+#
+#  id                :integer          not null, primary key
+#  name              :string
+#  email             :string
+#  twitter_handle    :string
+#  attended_meetups  :boolean
+#  living_in_chennai :boolean
+#  created_at        :datetime
+#  updated_at        :datetime
+#
+
 require 'test_helper'
 
 class CoachTest < ActiveSupport::TestCase
